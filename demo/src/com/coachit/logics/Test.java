517d86916179aaec656a1ef32838c318ed4e9734
rameshy007 <107032964+rameshy007@users.noreply.github.com>
2022-06-07 14:12:57 +0530

    Update Test.java
    remote commit

--- demo/src/com/coachit/logics/Test.java
+++ demo/src/com/coachit/logics/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("I am edited at remote");
 		System.out.println("I am local edited 3rd commit andd push");
+		System.out.println("ok I am remote");
 
 	}
 
