beae7ea13146613ef6ac05704dd452fdeff558be
rameshy007 <sheelamramesh454@gmail.com>
2022-06-07 14:15:31 +0530

    branch1 commit

--- demo/src/com/coachit/logics/Test.java
+++ demo/src/com/coachit/logics/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		System.out.println("I am edited at remote");
 		System.out.println("I am local edited 3rd commit andd push");
 		System.out.println("ok I am remote");
-
+		System.out.println("I am in branch1");
 	}
 
 }
